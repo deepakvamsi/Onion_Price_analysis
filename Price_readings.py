b606900289bacda0b872e7920e7d9fb2b8fff133
muskfan <34811671+muskfan@users.noreply.github.com>
2019-12-18 19:53:45 +0530

    Update Price_readings.py

--- Price_readings.py
+++ Price_readings.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import plotly.express as px
 import glob
-
-#pandas_bokeh.output_file("Interactive Plot.html")
-
+import imgkit as img
+import os
 
 file=glob.glob("Onion_20*.csv")
 
 for filename in file:
     df = pd.read_csv(filename,usecols=range(0, 8), low_memory=False) 
-    dataframe = df.loc[df['District'] == 'Nashik']
+    dataframe = df.loc[df['District'] == 'Kurnool']
     dataframe['Arrival_Date'] = pd.to_datetime(dataframe['Arrival_Date'])
     dframe = dataframe.sort_values(by='Arrival_Date', ascending=True)
-    #dframe.to_csv('onion_data.csv')
-    #daframe = pd.read_csv('onion_data.csv',usecols=range(0, 9),low_memory=False)
-    #line_in = figure(df, values = 'Arrival_Date', title = "Onion Prices Comparsion between years", legend= "top_left", Ylabel= "Maximum")
-    fig = px.bar(dframe, x = 'Arrival_Date', y = 'Max', hover_data=['Min', 'Market', 'Commodity', 'Variety'], color='Market', title='Onion quintal(100kgs) Price over time', labels={'Max':'Maximum Price/100Kg'}, width=1400,height=1000)
-    fig.show()
+    fig = px.bar(dframe, x = 'Arrival_Date', y = 'Max', hover_data=['Min', 'Market', 'Commodity', 'Variety'], color='Market', title='Onion quintal(100kgs) Price over time', labels={'Max':'Maximum Price/100Kg'}, width=1800,height=400)
+    file_name = os.path.splitext(filename)[0] 
+    html = "figure_%s"%file_name + ".html"
+    file_image = file_name + ".jpeg"
+    fig.write_html(html, auto_open=False)
+    img.from_file(html, file_image )
